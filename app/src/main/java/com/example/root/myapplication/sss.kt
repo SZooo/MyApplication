8927cae2da223b403d255358540b05b9702950b9
nicholas <kurode@sina.cn>
2017-12-13 15:48:45 +0800

    1. 提交git代码

---
+++ app/src/main/java/com/example/root/myapplication/sss.kt
@@ -0,0 +1,18 @@
+package com.example.root.myapplication
+
+import android.util.Log
+
+/**
+ * Created by root on 17-6-21.
+ */
+class sss
+{
+    lateinit var k: String
+
+    fun kTest()
+    {
+        Log.i("lvv", k.hashCode().toString())
+    }
+
+
+}
